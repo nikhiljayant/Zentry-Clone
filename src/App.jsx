fb3b93197081844fd7cd983d302213d8960f66a4
Nikhil Jayant <104789911+nikhiljayant@users.noreply.github.com>
2024-12-05 23:54:10 +0530

    about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 // Component
 import Hero from "./components/Hero";
+import About from "./components/About";
 
 const App = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
       <Hero />
+      <About />
     </main>
   );
 };
