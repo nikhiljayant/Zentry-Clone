c4d978d39b3c1d40aca9fd681f1ff9e9361ee04a
Nikhil Jayant <104789911+nikhiljayant@users.noreply.github.com>
2024-12-18 23:57:27 +0530

    features component

---
+++ src/components/Features.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Features = () => {
+  return (
+    <section className="bg-blue-200 pb-52">
+      <div className="container mx-auto px-3 md:px-10">
+        <div className="px-5 py-32">
+          <p className="font-circular-web text-lg text-blue-50">
+            Into the Metagame Layer
+          </p>
+        </div>
+        <p className="max-w-md font-circular-web text-lg text-blue-50 opacity-50">
+          Immerse youself in a rich and ever-expanding universe where a vibrant
+          array of products coverage into an interconnected overlay experience
+          on your world.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Features;
