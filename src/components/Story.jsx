02010ab299cb62ad3cbefdeee64250c148cc4990
Nikhil Jayant <104789911+nikhiljayant@users.noreply.github.com>
2024-12-23 23:30:58 +0530

    story image card and title

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -1,9 +1,81 @@
-import React from 'react'
+import React, { useRef } from "react";
+// Components
+import AnimatedTitle from "./AnimatedTitle";
+// GSAP
+import gsap from "gsap";
 
 const Story = () => {
-  return (
-    <div>Story</div>
-  )
-}
+  const frameRef = useRef(null);
 
-export default Story
+  const handleMouseLeave = () => {
+    const element = frameRef.current;
+
+    gsap.to(element, {
+      duration: 0.3,
+      ease: "power1.out",
+      rotateY: 0,
+      rotateX: 0,
+    });
+  };
+
+  const handleMouseMove = (e) => {
+    const { clientX, clientY } = e;
+    const element = frameRef.current;
+
+    if (!element) return;
+
+    const rect = element.getBoundingClientRect();
+    const x = clientX - rect.left;
+    const y = clientY - rect.top;
+
+    const centerX = rect.width / 2;
+    const centerY = rect.height / 2;
+
+    const rotateX = ((y - centerY) / centerY) * 10;
+    const rotateY = ((x - centerX) / centerX) * -10;
+
+    gsap.to(element, {
+      duration: 0.3,
+      ease: "power1.out",
+      transformPerspective: 500,
+      rotateY,
+      rotateX,
+    });
+  };
+
+  return (
+    <section className="min-h-dvh w-screen bg-blue-200 text-blue-50" id="story">
+      <div className="flex size-full flex-col items-center py-10">
+        <p className="font-general text-sm uppercase md:text-[10px]">
+          the multiversal ip world
+        </p>
+        <div className="relative size-full">
+          <AnimatedTitle
+            title="The st<b>o</b>ry of <br /> a hidden real<b>m</b>"
+            sectionId="#story"
+            containerClass="mt-5 pointer-events-none mix-blend-difference relative z-10"
+          />
+
+          <div className="story-img-container">
+            <div className="story-img-mask">
+              <div className="story-img-content">
+                <img
+                  ref={frameRef}
+                  onMouseLeave={handleMouseLeave}
+                  onMouseUp={handleMouseLeave}
+                  onMouseEnter={handleMouseLeave}
+                  onMouseMove={handleMouseMove}
+                  src="/img/entrance.webp"
+                  alt="entrance"
+                  className="object-contain"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Story;
