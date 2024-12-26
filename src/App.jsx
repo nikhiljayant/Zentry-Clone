a8673e18c0183f8cd37d31cc9223cf384def6cf1
Nikhil Jayant <104789911+nikhiljayant@users.noreply.github.com>
2024-12-26 23:26:52 +0530

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Navbar from "./components/Navbar";
 import Features from "./components/Features";
 import Story from "./components/Story";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
       <Features />
       <Story />
       <Contact />
+      <Footer />
     </main>
   );
 };
